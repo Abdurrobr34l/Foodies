9073ff3d8dd86952f01cd22e09f9c6d174915f49
AbdurRob <abdurrobr34l@gmail.com>
2024-11-11 19:32:53 +0600

    working on header section

--- js/script.js
+++ js/script.js
@@ -1,4 +1,26 @@
-// javascript
+// Javascript
+// TOGGLER START 
+// When menu link is clicked the hamburger menu turns off
+$('.js-scroll-trigger').click(function() {
+  $('.navbar-collapse').collapse('hide');
+});
+// When menu links is clicked the focus on toggler goes away 
+document.addEventListener("click", function (event) {
+  const toggler = document.querySelector(".navbar-toggler");
+  const isTogglerClick = toggler.contains(event.target);
+  const isMenuLink = event.target.closest(".navbar-nav .nav-link");
+
+  // If a menu link or area outside the toggler is clicked, blur the toggler
+  if (!isTogglerClick || isMenuLink) {
+      toggler.blur();
+  }
+});
+
+
+
+
+// TOGGLER END
+
 
 
 // jquery
